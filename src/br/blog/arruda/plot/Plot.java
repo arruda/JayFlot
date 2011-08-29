ef5cfc1ef9132a85f5bf83d9ef669245ebd3f11f
Felipe Arruda Pontes <arruda@uniriotec.com>
2011-08-29 19:40:11 -0300

    creating method that generate a basic plot, passing an PlotData array and the labels for the Plot

--- src/br/blog/arruda/plot/Plot.java
+++ src/br/blog/arruda/plot/Plot.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 
 import br.blog.arruda.plot.data.PlotData;
+import br.blog.arruda.plot.opt.PlotAxis;
 import br.blog.arruda.plot.opt.PlotOptions;
 
 import com.google.gson.Gson;
@@ -112,6 +113,33 @@ public class Plot {
 
 
 
+	/**
+	 * Generates a Default Plot using the two arrays passed for x and y axis.
+	 * Sets the labels of the axis for the labels passed in the parameters.
+	 * Using lines as option and no series or grid.
+	 * 
+	 * OBS:(this should only be used if using axisLabel plugin for FLOT) 
+	 * @param xAxis
+	 * @param yAxis
+	 * @return
+	 */
+	public static Plot generatePlot(ArrayList<PlotData>datas, String xLabel,String yLabel){
+		Plot plot = new Plot();
+		PlotOptions option = new PlotOptions(); 
+		PlotAxis xaxis = new PlotAxis();
+		PlotAxis yaxis = new PlotAxis();
+		
+		xaxis.setAxisLabel(xLabel);		
+		yaxis.setAxisLabel(yLabel);
+		
+		plot.options = option;
+		plot.options.setXaxis(xaxis);
+		plot.options.setYaxis(yaxis);
+		plot.datas = datas;
+		
+		return plot;
+	}
+	
 	// ================================== Metodos get() e set() ================================== //
 
 	public ArrayList<PlotData> getDatas() {
